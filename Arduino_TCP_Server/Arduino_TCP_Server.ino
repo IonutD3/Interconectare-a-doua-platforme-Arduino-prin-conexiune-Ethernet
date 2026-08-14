9e6a0031eee2eb99a1858af5459210f8f2ea8f4b
IonutD3 <146918885+IonutD3@users.noreply.github.com>
2026-08-14 18:55:06 +0300

    Add files via upload

--- Arduino_TCP_Server/Arduino_TCP_Server.ino
+++ Arduino_TCP_Server/Arduino_TCP_Server.ino
@@ -1,1 +1,61 @@
+#include <SPI.h>
+#include <Ethernet.h>
 
+// Portul TCP utilizat de server
+const int portServer = 4080;
+
+// Adresa MAC a platformei Arduino #2
+byte adresaMAC[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+
+// Crearea serverului TCP
+EthernetServer serverTCP(portServer);
+
+void setup() {
+  // Inițializarea comunicației seriale
+  Serial.begin(9600);
+
+  Serial.println("ARDUINO #2: SERVER TCP");
+
+  // Inițializarea modulului Ethernet și obținerea adresei IP prin DHCP
+  if (Ethernet.begin(adresaMAC) == 0) {
+    Serial.println("Eroare la configurarea Ethernet prin DHCP");
+  }
+
+  // Afișarea adresei IP primite de server
+  Serial.print("Adresa IP a serverului TCP: ");
+  Serial.println(Ethernet.localIP());
+
+  Serial.println("-> Introduceți această adresă în programul Arduino #1");
+
+  // Pornirea serverului TCP
+  serverTCP.begin();
+
+  Serial.print("Serverul TCP asculta pe portul: ");
+  Serial.println(portServer);
+}
+
+void loop() {
+  // Verificarea existenței unui client TCP
+  EthernetClient client = serverTCP.available();
+
+  // Dacă există un client conectat
+  if (client) {
+    // Citirea primului caracter transmis de client
+    char comanda = client.read();
+
+    Serial.print("- Comanda primita: ");
+    Serial.println(comanda);
+
+    // Verificarea existenței altor date disponibile
+    if (client.available() > 0) {
+      // Citirea caracterului primit
+      char caracterPrimit = client.read();
+
+      // Retransmiterea caracterului către client
+      serverTCP.write(caracterPrimit);
+
+      // Afișarea caracterului primit în Serial Monitor
+      Serial.write(caracterPrimit);
+    }
+  }
+}
